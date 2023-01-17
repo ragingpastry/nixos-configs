d06ab2581ffa4857037d118adbc7011704c69065
Nick Wilburn <senior.crepe@gmail.com>
2023-01-17 10:53:44 -0600

    feat: configure vscode

--- home/crepe/features/vscode/default.nix
+++ home/crepe/features/vscode/default.nix
@@ -1,23 +1,30 @@
-{ pkgs, config, ... }: {
-  home.packages = with pkgs;
-    [
-      (vscode-with-extensions.override {
-        vscodeExtensions = with vscode-extensions;
-          [ vscodevim.vim hashicorp.terraform ]
-          ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
-            {
-              name = "ansible";
-              publisher = "redhat";
-              version = "1.1.34";
-              sha256 = "UyAbQpe2KpoBZVk1AwfEr3BoLPwHxps284l0ZzjMQDE=";
-            }
-            {
-              name = "Nix";
-              publisher = "bbenoist";
-              version = "1.0.1";
-              sha256 = "qwxqOGublQeVP2qrLF94ndX/Be9oZOn+ZMCFX1yyoH0=";
-            }
-          ];
-      })
+{ pkgs, config, ... }:
+{
+  programs.vscode = {
+    enable = true;
+    package = pkgs.vscode;
+    enableExtensionUpdateCheck = false;
+    enableUpdateCheck = false;
+    mutableExtensionsDir = true;
+    extensions = with pkgs.vscode-extensions; [
+      vscodevim.vim
+      bbenoist.nix
+      hashicorp.terraform
+      dracula-theme.theme-dracula
+      ms-vsliveshare.vsliveshare
+    ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+      {
+        name = "ansible";
+        publisher = "redhat";
+        version = "1.1.34";
+        sha256 = "UyAbQpe2KpoBZVk1AwfEr3BoLPwHxps284l0ZzjMQDE=";
+      }
     ];
+    userSettings = {
+      "[nix]"."editor.tabSize" = 2;
+      "editor.fontFamily" = "${config.fontProfiles.monospace.family}";
+      "workbench.coloTheme" = "Monokai Dimmed";
+    };
+  };
+
 }
